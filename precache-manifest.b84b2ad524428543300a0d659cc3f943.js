82059dbc450702155887465d08e147119f7bdb96
manikathuria <manikathuria2001@gmail.com>
2020-10-05 01:11:03 +0530

    Updates

---
+++ precache-manifest.b84b2ad524428543300a0d659cc3f943.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a795110c8d75cdd3af8ef313979d38",
+    "url": "/ManinderKaur/index.html"
+  },
+  {
+    "revision": "2a4ff7c51da8834c8ad9",
+    "url": "/ManinderKaur/static/css/2.b59f41e4.chunk.css"
+  },
+  {
+    "revision": "66d40afc02bdb8921016",
+    "url": "/ManinderKaur/static/css/main.bcb97782.chunk.css"
+  },
+  {
+    "revision": "2a4ff7c51da8834c8ad9",
+    "url": "/ManinderKaur/static/js/2.cd9ec67d.chunk.js"
+  },
+  {
+    "revision": "46d12b19544a43695628e49c6affecc0",
+    "url": "/ManinderKaur/static/js/2.cd9ec67d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d40afc02bdb8921016",
+    "url": "/ManinderKaur/static/js/main.e3959883.chunk.js"
+  },
+  {
+    "revision": "9bf78b18da874a27410e",
+    "url": "/ManinderKaur/static/js/runtime-main.07d25d96.js"
+  },
+  {
+    "revision": "206f805e405309a7225b5e2683ffed19",
+    "url": "/ManinderKaur/static/media/abt1.206f805e.jpg"
+  },
+  {
+    "revision": "c61b65d1667d3e09e697a923da27a0b2",
+    "url": "/ManinderKaur/static/media/music2.c61b65d1.jpg"
+  },
+  {
+    "revision": "281f1f45532c0d3a862235756e76607c",
+    "url": "/ManinderKaur/static/media/news1.281f1f45.jpg"
+  },
+  {
+    "revision": "9a20f8b4d351fa34d889edc205e7830c",
+    "url": "/ManinderKaur/static/media/travel1.9a20f8b4.jpg"
+  }
+]);
